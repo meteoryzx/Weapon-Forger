995a856bdc6f3c54ebe56868c7bc744c7bb60653
JNKdog <3094919309@qq.com>
2026-09-14 14:10:58 +0800

    orient grinder belt toward operator

--- src/render/workshop-assets.ts
+++ src/render/workshop-assets.ts
@@ -40,10 +40,10 @@ export function grindingAsset(k:WorkshopModelKit):StationAsset {
   k.cylinder(root,"idler-hub",62,155,[0,430,0],"iron","x");
   // One closed abrasive loop. The front vertical run is the contact surface
   // and is intentionally exposed below the upper guard.
-  k.box(root,"belt-front",[96,760,32],[0,810,240],"brass",2);
-  k.box(root,"belt-back",[96,760,32],[0,810,-240],"brass",2);
-  k.box(root,"belt-top",[96,32,480],[0,1190,0],"brass",2);
-  k.box(root,"belt-bottom",[96,32,480],[0,430,0],"brass",2);
+  k.box(root,"belt-front",[32,760,480],[-240,810,0],"brass",2);
+  k.box(root,"belt-back",[32,760,480],[240,810,0],"brass",2);
+  k.box(root,"belt-top",[480,32,32],[0,1190,0],"brass",2);
+  k.box(root,"belt-bottom",[480,32,32],[0,430,0],"brass",2);
   k.box(root,"upper-guard",[190,180,300],[0,1375,0],"iron",2);
   k.box(root,"lower-guard",[190,120,300],[0,285,0],"iron",2);
   k.box(root,"contact-rest",[560,38,190],[0,760,300],"steel",0);
